641fbb4ddf25de0e4601c83990c00b228a84789d
¨vnaty09¨ <natalia.maria1008@gmail.com>
2026-04-13 12:24:49 +0200

    new

--- ghiceste numarul/ghicestenr.cpp
+++ ghiceste numarul/ghicestenr.cpp
@@ -5,32 +5,36 @@ using namespace std;
 
 int main()
 {
-    int  n,randomNumber,attempt=10,attemptUser=0;
+    int n,randomNumber,attempt=10,attemptUser=0;
     
     srand(time(0));
     randomNumber=rand()%101;
     cout<<"Welcome to Guess the number!"<<endl;
     cout<<"Enter a  number: "<<endl;
-    while(true)
+    while(attempt!=0)
     {cout<<"what's your guess"<<endl;
     cin>>n;
     attemptUser++;
-        if(randomNumber<attempt){
+        if(randomNumber<attemptUser){
             cout<<"Too high.Try again"<<endl;
             attempt--;
+            cout<<"You still have "<<attempt<<" attempts"<<endl; 
         }
         
         if(randomNumber>attemptUser){
-            cout<<"Too low"<<endl;
+            cout<<"Too low.Try again"<<endl;
             attempt--;
+            cout<<"You still have "<<attempt<<" attempts"<<endl;
         }
         
         if(randomNumber==attemptUser){
-            cout<<"Well done!That is the number!"<<endl;
-            attempt--;break;} 
+            cout<<"Well done!That's the number! You won the game!"<<endl;
+            attempt--;
+            cout<<"You guessed the number in "<<attempt<<" attempts"<<endl;
+            break;}
 
         if(attempt==0){
-            cout<<"No more attempts"<<endl;
+            cout<<"No more attempts. You lost"<<endl;
             return 0;
         }
     }
